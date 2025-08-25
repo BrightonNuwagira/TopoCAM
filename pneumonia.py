f4bb28250467dd8c01ad182859ca9701d12d545a
BrightonNuwagira <126713066+BrightonNuwagira@users.noreply.github.com>
2025-08-25 17:57:42 -0400

    Update pneumonia.py

--- pneumonia.py
+++ pneumonia.py
@@ -202,13 +202,15 @@ def compute_metrics(y_true, y_pred, y_prob):
         spec = np.mean(TN / (TN + FP + 1e-8))
     return dict(AUC=auc, Accuracy=acc, Precision=prec, Recall=rec, F1=f1, Sensitivity=sens, Specificity=spec)
 
-def evaluate_mlp(X_train, y_train, X_val, y_val, X_test, y_test):
+
+def evaluate_mlp(X_train, y_train, X_test_1, y_test_1):
     clf = MLPClassifier(hidden_layer_sizes=(100, 64), max_iter=500, random_state=42)
     clf.fit(X_train.reshape(len(X_train), -1), y_train.ravel())
-    y_pred = clf.predict(X_test.reshape(len(X_test), -1))
-    y_prob = clf.predict_proba(X_test.reshape(len(X_test), -1))
+    y_pred = clf.predict(X_test_1.reshape(len(X_test_1), -1))
+    y_prob = clf.predict_proba(X_test_1.reshape(len(X_test_1), -1))
     y_score = y_prob[:, 1] if y_prob.shape[1] == 2 else y_prob
-    return compute_metrics(y_test.ravel(), y_pred, y_score)
+    return compute_metrics(y_test_1.ravel(), y_pred, y_score)
+
 
 # Initialize model
 model = ResNet2DNoInplace(n_classes).to(device)
@@ -264,27 +266,6 @@ bv_normcount_test = append_pixel_count_to_normalized(bv_test, seg_test)
 pd.DataFrame(bv_normcount_train).to_csv("Nonormalization_pneumonia_imagemasked_normcount_train.csv", index=False)
 pd.DataFrame(bv_normcount_val).to_csv("Nonormalization_pneumonia_imagemasked_normcount_val.csv", index=False)
 pd.DataFrame(bv_normcount_test).to_csv("Nonormalization_pneumonia_imagemasked_normcount_test.csv", index=False)
-
-
-
-
-
-
-
-# Evaluate and save results
-results = []
-results.append({
-    "Model": "MLP_Betti_NormalizedPlusPixelCount",
-    **evaluate_mlp(bv_normcount_train, y_train, bv_normcount_val, y_val, bv_normcount_test, y_test)
-})
-
-
-pd.DataFrame(results).to_csv("Nonormilization_pneumonia_normcount_results.csv", index=False)
-print("Saved: breast_normcount_results.csv")
-
-
-
-
 
 
 
